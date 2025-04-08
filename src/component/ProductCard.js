53271a7178248c041ce4484c4c619facfc532780
lliwIWant <liwil5486@gmail.com>
2025-04-08 09:40:53 +0900

    shopping header

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://image.hm.com/assets/hm/56/30/563034f6124f27a712cef030cf78f1cf8fd33d33.jpg?imwidth=564'/>
+        <div>Conscious choice</div>
+        <div>벨티드 트윌 코드</div>
+        <div>99900</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ src/page/ProductAll.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
+
+const ProductAll = () => {
+    const [productList, setProductList] = useState([]);
+    const getProduct  = async()=>{
+        let url = `http://localhost:5000/products`;
+        let response = await fetch(url);
+        let data = await response.json();
+        // console.log(data);
+        setProductList(data);
+    };
+    
+    useEffect(()=>{
+        getProduct();
+    },[])
+
+  return (
+    <div>
+        <ProductCard/>
+    </div>
+  )
+}
+
+export default ProductAll
