734a938a11690fd96b97d295e1fff88cdea1a3df
lliwIWant <liwil5486@gmail.com>
2025-04-09 14:33:47 +0900

    jsonserver

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,7 +5,7 @@ import {Row, Container, Col} from 'react-bootstrap';
 const ProductAll = () => {
     const [productList, setProductList] = useState([]);
     const getProduct  = async()=>{
-        let url = `http://localhost:5000/products`;
+        let url = `https://my-json-server.typicode.com/lliwIWant/shopping-mall-sub/products`;
         let response = await fetch(url);
         let data = await response.json();
         // console.log(data);
