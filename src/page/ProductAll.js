e7ec2399dfb12c25929281f87d62fe9f7a59496f
lliwIWant <liwil5486@gmail.com>
2025-04-09 15:13:35 +0900

    asdf

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,7 +5,7 @@ import {Row, Container, Col} from 'react-bootstrap';
 const ProductAll = () => {
     const [productList, setProductList] = useState([]);
     const getProduct  = async()=>{
-        let url = `http://my-json-server.typicode.com/lliwIWant/shoppingMall/products`;
+        let url = `https://my-json-server.typicode.com/lliwIWant/shoppingMall/products`;
         let response = await fetch(url);
         let data = await response.json();
         // console.log(data);
